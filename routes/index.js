e483bb4dc116d406713550982db791a80f8d9908
Sondre <sondresit@gmail.com>
2017-12-10 13:35:25 +0100

    update navbar, update routes to register/login and campground index

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get("/", function (req, res) {
 // AUTH ROUTES
 // show register form
 router.get("/register", function (req, res) {
-    res.render("register");
+    res.render("register", {page: "register"});
 });
 
 //handle signup logic
@@ -30,7 +30,7 @@ router.post("/register", function (req, res) {
 
 //show login form
 router.get("/login", function (req, res) {
-    res.render("login");
+    res.render("login", {page: "login"});
 })
 
 router.post("/login", passport.authenticate("local",
